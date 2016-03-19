86b3b78dae919f35c15b59b6d3c9d147c68612d9
Jonathan Chu <jonathan.chu@me.com>
2016-03-18 23:22:15 -0400

    Add empty color theme file to start

---
+++ octocat-theme.el
@@ -0,0 +1,20 @@
+;;; octocat-theme.el --- Octocat color theme
+;;
+;; Copyright 2016 Jonathan Chu
+;;
+;; Author: Jonathan Chu <me@jonathanchu.is>
+;; URL: https://github.com/jonathanchu/octocat-theme
+;; Version: 0.1.0
+;;
+;;; Commentary:
+;;
+;; An Octocat theme for Emacs.
+;;
+;;; Code:
+
+;; color theme code goes here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+;;; octocat-theme.el ends here
